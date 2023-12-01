a7c6559eafecb42ec66a3a5a00640948d0c6cfc4
nilendrapatel1317 <nilendrapatel1317@gmail.com>
2023-12-01 16:08:07 +0530

    Channel Id Page and Member Id Page is created ,  Done.

--- app/(main)/(routes)/servers/[serverId]/page.js
+++ app/(main)/(routes)/servers/[serverId]/page.js
@@ -1,12 +1,45 @@
-import { UserButton } from "@clerk/nextjs";
-import React from "react";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const page = () => {
-  return (
-    <>
-      <div>Server ID Page</div>
-    </>
-  );
+import { currentProfile } from "@/lib/currentProfile";
+import { db } from "@/lib/db";
+
+const ServerIdPage = async ({ params }) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== "general") {
+    return null;
+  }
+
+  const member = await db.member.findFirst({
+    where: {
+      profileId: profile.id,
+    },
+  });
+
+  return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`);
 };
 
-export default page;
+export default ServerIdPage;
