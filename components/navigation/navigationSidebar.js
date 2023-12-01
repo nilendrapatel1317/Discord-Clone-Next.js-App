0fd6507598c757d714cc63c19d0f97c28e57b0bd
nilendrapatel1317 <nilendrapatel1317@gmail.com>
2023-12-02 02:20:43 +0530

    Chat Scroll Hook ( starting chat div is from bottom) Part DONE 😊

--- components/navigation/navigationSidebar.js
+++ components/navigation/navigationSidebar.js
@@ -62,7 +62,7 @@ export const NavigationSidebar = async () => {
     })
     .filter((server) => server.members.length > 0);
 
-  // console.log(filteredServers);
+  
 
   return (
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] bg-[#E3E5E8] py-3">
