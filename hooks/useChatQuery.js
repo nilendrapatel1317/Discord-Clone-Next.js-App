54757a11dd35de940dd67d53a83e08a59d173398
nilendrapatel1317 <nilendrapatel1317@gmail.com>
2023-12-02 15:39:58 +0530

    some changes

--- hooks/useChatQuery.js
+++ hooks/useChatQuery.js
@@ -27,7 +27,8 @@ export const useChatQuery = ({ queryKey, apiUrl, paramKey, paramValue }) => {
       queryKey: [queryKey],
       queryFn: fetchMessages,
       getNextPageParam: (lastPage) => lastPage?.nextCursor,
-      refetchInterval: isConnected ? false : 1000,
+      refetchInterval: 1000,
+      // refetchInterval: isConnected ? false : 1000,
     });
 
   return {
