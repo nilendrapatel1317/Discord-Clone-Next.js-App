b05ebd68f5a14212f6520723719fc84c33667ff7
nilendrapatel1317 <nilendrapatel1317@gmail.com>
2024-01-01 18:04:19 +0530

    dfg

--- middleware.js
+++ middleware.js
@@ -2,6 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
   publicRoutes: ["/", "/(api|trpc)(.*)", "/api/uploadthing"],
+  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)", "/servers/6575f8f344763e664a04ff95"]
 });
  
 export const config = {
